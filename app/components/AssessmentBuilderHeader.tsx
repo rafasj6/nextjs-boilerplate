02b227e1ada77a3227cc2d621896b2c89667512d
rafasj6 <rafael.jacobovitz@gmail.com>
2024-11-11 22:19:04 -0500

    feat: toast when save

--- app/components/AssessmentBuilderHeader.tsx
+++ app/components/AssessmentBuilderHeader.tsx
@@ -1,15 +1,19 @@
 import { useSaveAssessment } from "../hooks/assessmentHooks";
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export function AssessmentBuilderHeader() {
-    const saveAssessment = useSaveAssessment().mutate;
-  
-    return <div className="w-full flex justify-between">
-      <p className="text-xl font-bold">Python Developer</p>
-      <button
-        className="bg-[#217BBB] text-white rounded-md px-2 py-1"
-        onClick={() => saveAssessment()} >
-        <p>Save</p>
-      </button>
-    </div>
-  }
-  
+  const saveAssessment = useSaveAssessment().mutate;
+  const handleSave = () => {
+    saveAssessment();
+    toast.success("Assessment saved successfully!");
+  };
+  return <div className="w-full flex justify-between">
+    <p className="text-xl font-bold">Python Developer</p>
+    <button
+      className="bg-[#217BBB] text-white rounded-md px-2 py-1"
+      onClick={handleSave} >
+      <p>Save</p>
+    </button>
+  </div>
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 "use client"
 import { QueryClient, QueryClientProvider } from "react-query";
 import { AsssementComponent } from "./components/AsssementComponent";
+import { ToastContainer } from 'react-toastify';
 
 
 export default function Home() {
   const queryClient = new QueryClient();
   return (
     <QueryClientProvider client={queryClient}>
+      <ToastContainer />
       <AsssementComponent />
     </QueryClientProvider>
   );
