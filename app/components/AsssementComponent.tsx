640b9bed2fd53e450098d423efe2dc24439d0972
rafasj6 <rafael.jacobovitz@gmail.com>
2024-11-11 21:18:09 -0500

    reset highlighted question when loading

--- app/components/AsssementComponent.tsx
+++ app/components/AsssementComponent.tsx
@@ -5,21 +5,22 @@ import { AssessmentBuilder } from "./AssessmentBuilder";
 import { AssessmentBuilderHeader } from "./AssessmentBuilderHeader";
 
 export function AsssementComponent() {
-    const setBlob = useAssessmentStore((state) => state.setBlob);
-    const { data: blob } = useFetchAssessment();
-  
-    useEffect(() => {
-      if (blob) {
-        setBlob(blob);
-      }
-    }, [blob])
-  
-    return <div className="flex flex-col gap-10 bg-gray-50 w-screen min-h-screen">
-      <div className="w-full h-10 bg-[#123A68]" />
-      <div className="text-black  px-20 flex flex-col gap-10">
-        <AssessmentBuilderHeader />
-        <AssessmentBuilder />
-      </div>
+  const setBlob = useAssessmentStore((state) => state.setBlob);
+  const setHighlightedQuestion = useAssessmentStore((state) => state.setHighlightedQuestion);
+  const { data: blob } = useFetchAssessment();
+
+  useEffect(() => {
+    if (blob) {
+      setBlob(blob);
+      setHighlightedQuestion(0,0)
+    }
+  }, [blob])
+
+  return <div className="flex flex-col gap-10 bg-gray-50 w-screen min-h-screen">
+    <div className="w-full h-10 bg-[#123A68]" />
+    <div className="text-black  px-20 flex flex-col gap-10">
+      <AssessmentBuilderHeader />
+      <AssessmentBuilder />
     </div>
-  }
-  
+  </div>
+}
